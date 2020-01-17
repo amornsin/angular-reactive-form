724a5f0bfb1099656bbc56b5e49a72159e02335b
Amornsin K <gigy14541@gmail.com>
2020-01-17 07:43:33 +0700

    add project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.profileForm.value);
+  }
+
+}
